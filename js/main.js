fff7b01fea7ba973279ade5072955691eb46f2a0
Faith Ochonu Dawah <faitech.tribes@gmail.com>
2024-03-29 22:11:36 +0100

    External Styling explained and forms

--- js/main.js
+++
@@ -1,9 +0,0 @@
-confirm("are you sure you want to delete");
-alert("external script");
-prompt("what is yur name");
-
-const nam = prompt("what is your name");
-console.log(nam, "name");
-//hjkkssks
-/* this is a multiple line statement*/
-// data types
