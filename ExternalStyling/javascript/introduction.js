d76a8e99bf01779f3d6f32671a33991995bfd201
Faith Ochonu Dawah <faitech.tribes@gmail.com>
2024-04-24 12:58:39 +0100

    introduction to javascript class

---
+++ ExternalStyling/javascript/introduction.js
@@ -0,0 +1,34 @@
+let firstname = "joy";
+let lastName = "peter";
+let country = "Nigeria";
+let state = "Benue";
+console.log(firstname);
+
+//primitive datatypes
+let numOne = 3;
+let numTwo = 3;
+console.log(numOne == numTwo); //true
+
+//non-primitive datatypes
+let sum = [1, 2, 3];
+let sums = [1, 2, 3];
+console.log(sum == sums); //false
+
+// maths object
+Math.E; // returns Euler's number
+Math.PI; // returns PI
+Math.SQRT2; // returns the square root of 2
+Math.SQRT1_2; // returns the square root of 1/2
+Math.LN2; // returns the natural logarithm of 2
+Math.LN10; // returns the natural logarithm of 10
+Math.LOG2E; // returns base 2 logarithm of E
+Math.LOG10E; // returns base 10 logarithm of E
+
+//VARIABLES
+//STRINGS
+let carName1 = "Volvo XC60"; // Double quotes
+let carName2 = "Volvo XC60"; // Single quotes
+let carName3 = `benz`;
+let cars =
+  "I love " + carName1 + ", " + "but not " + carName2 + " and " + carName3;
+console.log(cars);
